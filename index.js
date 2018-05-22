e9efcdfd0800b0793447afeb2398f44181c2b7d3
aonemd <aonemdsaleh@gmail.com>
2018-05-22 14:07:48 +0200

    Add issue comments API

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const express = require('express');
+const graphql = require('graphql.js')
 
-const app = express();
+const app          = express();
+const access_token = process.env.GH_ACCESS_TOKEN
+const owner        = process.env.OWNER
+const repo         = process.env.REPO
 
 app.set('port', (process.env.PORT || 8001));
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/issues/:number/comments', (req, res) => {
+  let graph = graphql("https://api.github.com/graphql", {
+    asJSON: true,
+    headers: {
+      "Authorization": `Bearer ${access_token}`,
+      "User-Agent": `${repo}`
+    }
+  })
+
+  graph(`
+query ($owner: String!, $repo: String!, $issue: Int!) {
+  repository(owner: $owner, name: $repo) {
+    issue(number: $issue) {
+      url
+      comments(first: 100) {
+        edges {
+          node {
+            body
+            createdAt
+            author {
+              login
+              avatarUrl
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+}`,
+    {
+      owner: owner,
+      repo: repo,
+      issue: parseInt(req.params.number)
+    }).then(function (result) {
+      res.status(200)
+      res.json(result)
+    }, function (err) {
+      res.status(403)
+      res.json(err)
+    });
+})
 
 app.listen(app.get('port'), () => console.log(`App listening on port ${app.get('port')}`))
